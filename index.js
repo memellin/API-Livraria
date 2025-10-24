e77832b564e63f9de0cf2f1251257364c092abaa
izoantonio-max <izoantonio@gmail.com>
2025-10-24 21:01:15 +0000

    Ajuste de porta para deploy

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// Importa as rotas de usuários
+const userRoutes = require("./routes/users");
+app.use("/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "API rodando dentro do Codespaces!" });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
